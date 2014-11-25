26f883e53a2f278380af8ee22af73dae0ffe6ab8
Adrián Mouly <adrian@mouly.com.ar>
2014-11-24 22:05:30 -0300

    Adding boostrap files.

--- server.js
+++ server.js
@@ -9,24 +9,25 @@ var server = http.createServer(function (request, response) {
     
     // Read template file
     fs.readFile('index.html', function (err, data) {
-
-
-
-        exec("cat /sys/class/thermal/thermal_zone0/temp", function (error, stdout, stderr) {
-            var temp;
-
-            if (error !== null) {
-                console.log('exec error: ' + error);
-            } else {
-                temp = parseFloat(stdout) / 1000;
-
-                response.writeHead(200, { 'Content-Type': 'text/html', 'Content-Length': data.length });
-                response.write(data + temp);
-                response.end();
-            }
-        });
+        response.writeHead(200, { 'Content-Type': 'text/html', 'Content-Length': data.length });
+        response.write(data);
+        response.end();
     });
 });
+
+function readTemp() {
+    'use strict';
+
+    var temp;
+
+    exec("cat /sys/class/thermal/thermal_zone0/temp", function (error, stdout, stderr) {
+        if (error !== null) {
+            console.log('exec error: ' + error);
+        } else {
+            temp = parseFloat(stdout) / 1000;
+        }
+    });
+}
 
 // Listen on port
 server.listen(8000);
